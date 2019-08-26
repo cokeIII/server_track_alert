286a6ee4cc79bcf366ff7f4c0c37a8ec19b0dc6c
cokeIII <coke2484@gmail.com>
2019-08-26 22:11:20 +0700

    test

---
+++ index.js
@@ -0,0 +1,50 @@
+var mysql      = require('mysql');
+var express    = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'track_alert'
+});
+var app = express();
+app.use(bodyParser.json());
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+app.get("/maps",function(req,res){
+connection.query('SELECT * from maps', function(err, rows, fields) {
+  if (!err){
+    var data = {
+      maps:rows
+    }
+    //console.log('The solution is: ', rows);
+    res.json(data);
+  }
+  else
+    console.log('Error while performing Query.');
+  });
+});
+
+app.post("/insertUser",function(req,res){
+  connection.query('insert into users (user_id,name,phone_number,device_id) values("'+req.body.idCard+'","'+req.body.userName+'","'+req.body.phoneNumber+'","'+req.body.deviceId+'")', function(err, result) {
+    if (!err){
+      console.log(req.body)
+      res.json({status: "OK"});
+    }
+    else
+      console.log('Error while performing Query.');
+    });
+  });
+
+
+app.listen(3001);
+// connection.end();
